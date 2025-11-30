7eee4f4503105fa5a5e14a01aa77d5cc794d78ed
sagarOsammy <99390892+sagarOsammy@users.noreply.github.com>
2025-11-30 14:02:09 +0530

    app:2.0

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+           <Route path="/battlefield-6-mockups/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
